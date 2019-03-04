995b491a3cdade9bd21cc9bbf2cda1b4e16dbdc2
Nwakuso <1808891@rgu.ac.uk>
2019-03-04 10:38:37 +0000

    Initial commit

---
+++ PHPStormProjects/Lab8/Index.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_SERVER','CSDM-WEBDEV');
+define('DB_USERNAME','1808891');
+define('DB_PASSWORD','1808891');
+define('DB_DATABASE','db1808891_cmm007')
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
---
+++ PHPStormProjects/Lab8/Login.php
@@ -0,0 +1,18 @@
+<?php
+define ('DB_SERVER', 'CSDM-WEBDEV');
+define ('DB_USERNAME', '1808891');
+define ('DB_PASSWORD', '1808891');
+define ('DB_DATABASE', 'db1808891_cmm007')
+
+$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+
+if ($db==false)
+{
+    die("ERROR: could not connect. " . mysqli_connect_error());
+}
+
+else
+{
+    echo "Successfull";
+}
+?>
